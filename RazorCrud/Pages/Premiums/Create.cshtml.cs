995a39b662dbb59077f6cee531fece96696011e6
Jonatas Viana <jonatas.viana@kria.com.br>
2024-04-09 18:54:48 -0300

    feat: criando area de aluno premium

---
+++ RazorCrud/Pages/Premiums/Create.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorCrud.Data;
+using RazorCrud.Models;
+
+namespace RazorCrud.Pages_Premiums
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorCrud.Data.ApplicationDbContext _context;
+
+        public CreateModel(RazorCrud.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Email");
+            return Page();
+        }
+
+        [BindProperty]
+        public Premium Premium { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || _context.Premiums == null || Premium == null)
+            {
+                return Page();
+            }
+
+            _context.Premiums.Add(Premium);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
