5ab97663dd07b02d44ca731367156e388d5a53ac
Jonatas Viana <jonatas.viana@kria.com.br>
2024-04-09 17:41:44 -0300

    feat: adicionando modelagens

---
+++ RazorCrud/Models/Premium.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorCrud.Models
+{
+    public class Premium
+    {
+        [Key]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Título é obrigatório.")]
+        [StringLength(80, ErrorMessage = "O campo Título deve ter no máximo 80 caracteres.")]
+        [MinLength(5, ErrorMessage = "O campo Título deve ter no mínimo 5 caracteres.")]
+        [DisplayName("Título")]
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Data de Início")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("Data de Término")]
+        public DateTime EndDate { get; set; }
+
+        [DisplayName("Aluno")]
+        [Required(ErrorMessage = "O campo Aluno é obrigatório.")]
+        public int StudentId { get; set; }
+        public Student? Student { get; set; }
+    }
+}
---
+++ RazorCrud/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorCrud.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(80, ErrorMessage = "O campo Nome deve ter no máximo 80 caracteres.")]
+        [MinLength(5, ErrorMessage = "O campo Nome deve ter no mínimo 5 caracteres.")]
+        [Display(Name = "Nome Completo")]
+        public string Nome { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; } = String.Empty;
+
+        public List<Premium> Premiums { get; set; } = new();
+
+
+    }
+}
